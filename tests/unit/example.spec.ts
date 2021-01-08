c32152e0a60eb6f41daf40559c6dca75394e5eed
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-01-08 07:57:26 +0200

    Prepared app layout

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Timer from '@/components/Timer.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Timer.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
